23150604d8be2f903c3f0b34b8ea95dc5061b304
JSLittleChicken <oksana.petrovich.2019@gmail.com>
2023-11-26 20:16:20 +0200

    hw8 finished

---
+++ hw8.js
@@ -0,0 +1,6 @@
+let a = +prompt("Введіть перше число");
+let b = +prompt("Введіть друге число");
+let c = +prompt("Введіть третє число");
+let numbers = [a, b, c];
+let sum = (a+b+c);
+alert(sum/numbers.length);
